5200ca0b3b3c69b11225b1fa8b38d13ee4bc80c4
Jan Bures <buress.jan@email.cz>
2023-01-31 12:15:54 +0100

    Consent: fix accept rendering

--- src/components/consent/Consent.tsx
+++ src/components/consent/Consent.tsx
@@ -4,38 +4,31 @@ import { LteTabletCheck } from '../../theme/MediaQueries';
 import theme from '../../theme/theme';
 
 const Consent = () => {
-  const [accepted, setAccepted] = useState<boolean>(false);
-
   const handleAccept = () => {
-    setAccepted(true);
     Cookies.set('cookie-consent', true, { expires: 150 });
   };
 
   return (
-    <>
-      {!accepted && (
-        <CookieConsent
-          location='bottom'
-          buttonText='Rozumím'
-          cookieName='cookie-consent'
-          style={{
-            fontSize: `${LteTabletCheck() ? '10px' : '14px'}`,
-            background: `${theme.colors.dark}`,
-          }}
-          buttonStyle={{
-            background: `${theme.colors.ice}`,
-            color: `${theme.colors.black}`,
-            marginTop: `${LteTabletCheck() && '0px'}`,
-            fontSize: `${LteTabletCheck() ? '10px' : '12px'}`,
-          }}
-          expires={150}
-          onAccept={handleAccept}
-        >
-          Tyto webové stránky používají soubory cookie ke zlepšení uživatelského
-          zážitku
-        </CookieConsent>
-      )}
-    </>
+    <CookieConsent
+      location='bottom'
+      buttonText='Rozumím'
+      cookieName='cookie-consent'
+      style={{
+        fontSize: `${LteTabletCheck() ? '12px' : '14px'}`,
+        background: `${theme.colors.dark}`,
+      }}
+      buttonStyle={{
+        background: `${theme.colors.ice}`,
+        color: `${theme.colors.black}`,
+        marginTop: `${LteTabletCheck() && '0px'}`,
+        fontSize: `${LteTabletCheck() ? '10px' : '12px'}`,
+      }}
+      expires={150}
+      onAccept={handleAccept}
+    >
+      Tyto webové stránky používají soubory cookie ke zlepšení uživatelského
+      zážitku
+    </CookieConsent>
   );
 };
 
