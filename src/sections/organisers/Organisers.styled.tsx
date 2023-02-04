b95c6e0706b6d1077e430357651ad92cb0cb7a40
Jan Bures <buress.jan@email.cz>
2023-02-04 12:35:19 +0100

    Remove onScroll reveal on Organisers

--- src/sections/organisers/Organisers.styled.tsx
+++ src/sections/organisers/Organisers.styled.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import theme from '../../theme/theme';
+import styled from "styled-components";
+import theme from "../../theme/theme";
 
 export const OrganiserWrapper = styled.div`
   display: flex;
@@ -11,6 +11,22 @@ export const OrganiserWrapper = styled.div`
     flex-direction: column;
     padding-top: 3em;
     gap: 2em;
+  }
+`;
+
+export const OrganisersHeadline = styled.span`
+  max-width: 80%;
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizeElements.h1}px;
+  text-align: center;
+  color: ${theme.colors.black};
+
+  @media screen and (${theme.breakpoints.tabletQuery}) {
+    font-size: ${theme.fontSizeElements.h2}px;
+  }
+  @media screen and (${theme.breakpoints.mobileQuery}) {
+    max-width: 90%;
+    font-size: ${theme.fontSizeElements.h3}px;
   }
 `;
 
