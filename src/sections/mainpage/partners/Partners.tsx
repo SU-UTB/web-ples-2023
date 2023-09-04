08dd7cc6b829653b957d20bab448a268d09accff
Filip Tomes <filiptomes@protonmail.com>
2023-09-04 17:11:47 +0200

    fix: fucked up conflicts

--- src/sections/mainpage/partners/Partners.tsx
+++ src/sections/mainpage/partners/Partners.tsx
@@ -1,25 +1,3 @@
-import Headline from '../../../components/headline/Headline';
-import { WrapperSection } from '../../../components/layout/wrapper/Wrapper.styled';
-import { partners } from './Partners.content';
-import { PartnerImg, PartnerLogo, PartnerWrapper } from './Partners.styled';
-
-const Partners = () => {
-  return (
-    <WrapperSection centered id='partneri'>
-      <Headline color='black'>Partneři</Headline>
-      <PartnerWrapper>
-        {partners.map((item, index) => (
-          <PartnerLogo key={index} className='reveal reveal-slow'>
-            <PartnerImg src={item.logoSrc} alt={item.name} />
-          </PartnerLogo>
-        ))}
-      </PartnerWrapper>
-    </WrapperSection>
-  );
-};
-
-export default Partners;
-
 import flyUnited from '../../../assets/images/partners/generalPartner/flyUnited.svg';
 import Headline from '../../../components/headline/Headline';
 import { WrapperSection } from '../../../components/layout/wrapper/Wrapper.styled';
